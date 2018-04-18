83ade2c111a82304ce2512c200c7d37d5cbdd127
richard <rtasker@vqcomms.com>
2018-04-18 23:39:32 +0100

    Add ResponseNegotiator and Sample Project

---
+++ sample/Features/Home/HomeModule.cs
@@ -0,0 +1,23 @@
+﻿namespace Carter.SirenNegotiator.Sample
+{
+    using System;
+    using Carter;
+    using Microsoft.AspNetCore.Http;
+
+    public class HomeModule : CarterModule
+    {
+        public HomeModule()
+        {
+            Get("/", async (req, res, routeData) =>
+            {
+                res.StatusCode = 409;
+                await res.WriteAsync("There's no place like 127.0.0.1");
+            });
+
+            After = async ctx =>
+            {
+                await Console.Out.WriteLineAsync("Catch you later!");
+            };
+        }
+    }
+}
---
+++ src/SirenResponseNegotiator.cs
@@ -0,0 +1,46 @@
+﻿namespace Carter.SirenNegotiator
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Net.Http.Headers;
+    using Response;
+
+    public class SirenResponseNegotiator : IResponseNegotiator
+    {
+        private readonly IServiceProvider serviceProvider;
+
+        public SirenResponseNegotiator(IServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+        }
+        
+        public bool CanHandle(MediaTypeHeaderValue accept)
+        {
+            return IsExactMatch(accept.MediaType.ToString()) 
+                   || IsPartialMatch(accept.SubType.ToString());
+        }
+
+        public async Task Handle(HttpRequest req, HttpResponse res, object model, CancellationToken cancellationToken)
+        {
+            var handlerType = typeof(ISirenDocumentWriter<>).MakeGenericType(model.GetType());
+            var docWriter = serviceProvider.GetService(handlerType);
+            
+            var sirenRes = docWriter.Write(model, new Uri(req.Path.ToString()));  // Hmmmm! Can't resolve the type properly
+
+            await res.AsJson(sirenRes);
+        }
+        
+        private bool IsExactMatch(string mediaType)
+        {
+            return mediaType.Contains("application/vnd.siren+json");
+        }
+
+        private bool IsPartialMatch(string subType)
+        {
+            return subType.StartsWith("vnd", StringComparison.OrdinalIgnoreCase) 
+                   && subType.EndsWith("siren+json", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
